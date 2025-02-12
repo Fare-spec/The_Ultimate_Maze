b9624effb90895dbc842000b51b3aa1c2645e509
Spectre <elouan.fare@protonmail.ch>
2025-02-12 14:05:17 +0100

    finished the DFS function

--- src/maze.py
+++ src/maze.py
@@ -1,6 +1,8 @@
 import liste as fifo
 import random as rnd
 import pile as lifo
+rnd.seed(11)
+
 class Maze:
     def __init__(self, width, height):
         self.width = width
@@ -44,19 +46,37 @@ class Maze:
     
     def is_visited(self,x,y):
         return self.grille[y][x]['visited']
+    
 
+    def identify_direction(self,x1,y1,x2,y2):
+        if x1 == x2:
+            if y1 == y2+1:
+                return 'N','S'
+            else:
+                return 'S','N'
+        elif x1 == x2+1:
+            return 'W','E'
+        else:
+            return 'E','W'
 
 
     def dfs_generation(self, x_debut, y_debut):
+        self.grille[y_debut][x_debut]['visited'] = True
         chemin = lifo.creer_pile_vide()
-        chemin = lifo.empiler(chemin, (x_debut,y_debut))
+        chemin = lifo.empiler(chemin, (x_debut, y_debut))
         while not lifo.est_pile_vide(chemin):
-            voisins = self.get_voisins(x_debut, y_debut)
-            unvisited = []
-            for i in voisins:
-                if not self.is_visited(i[0],i[1]):
-                    unvisited.append(i)
-            rnd.choice(unvisited)
+            current = lifo.sommet(chemin)
+            voisins = self.get_voisins(current[0], current[1])
+            unvisited = [voisin for voisin in voisins if not self.is_visited(voisin[0], voisin[1])]
+            if unvisited:
+                selected_voisin = rnd.choice(unvisited)
+                murs = self.identify_direction(current[0], current[1], selected_voisin[0], selected_voisin[1])
+                self.delete_wall(current[0], current[1], murs[0])
+                self.delete_wall(selected_voisin[0], selected_voisin[1], murs[1])
+                self.grille[selected_voisin[1]][selected_voisin[0]]['visited'] = True
+                chemin = lifo.empiler(chemin, selected_voisin)
+            else:
+                chemin = lifo.depiler(chemin)
 
 
 a = Maze(10,10)
