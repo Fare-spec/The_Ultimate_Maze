7999f03a96ce64e0ab0e1fdad34f863737a4ce7d
Spectre <elouan.fare@protonmail.ch>
2025-02-13 14:40:13 +0100

    Auto urgent commit.

---
+++ The_Ultimate_Maze/src/main.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Cell {
+    visited: bool,
+    walls: HashMap<char, bool>,
+}
+
+#[derive(Debug)]
+struct Maze {
+    grille: Vec<Vec<Cell>>,
+    width: usize,
+    height: usize,
+}
+
+impl Maze {
+    fn new(height: usize, width: usize) -> Maze {
+        let grille: Vec<Vec<Cell>> = (0..height)
+            .map(|_| {
+                (0..width)
+                    .map(|_| {
+                        let mut walls = HashMap::new();
+                        walls.insert('N', true);
+                        walls.insert('S', true);
+                        walls.insert('E', true);
+                        walls.insert('W', true);
+
+                        Cell {
+                            visited: false,
+                            walls,
+                        }
+                    })
+                    .collect()
+            })
+            .collect();
+
+        Maze {
+            grille,
+            width,
+            height,
+        }
+    }
+}
+
+fn main() {
+    let maze = Maze::new(5, 5);
+    println!("{:#?}", maze);
+}
---
+++ The_Ultimate_Maze/src/pile.rs
@@ -0,0 +1,24 @@
+pub struct Stack<T> {
+    items: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    pub fn new() -> Self {
+        Self { items: Vec::new() }
+    }
+    pub fn push(&mut self, item: T) {
+        self.items.push(item);
+    }
+    pub fn pop(&mut self) -> Option<T> {
+        self.items.pop()
+    }
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+    pub fn peek(&self) -> Option<&T> {
+        self.items.last()
+    }
+    pub fn size(&self) -> usize {
+        self.items.len()
+    }
+}
