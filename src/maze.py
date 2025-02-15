43eae90a1322cc3473b89062dd2b699bc7bc50d4
spectre <elouan.fare@protonmail.ch>
2025-02-15 22:08:30 +0100

    change width and height b

--- src/maze.py
+++ src/maze.py
@@ -15,47 +15,39 @@ class Maze:
                     'walls': {'N': True, 'S': True, 'E': True, 'W': True},
                     'teleporter': False,
                 }
-                for _ in range(self.width+1)
+                for _ in range(self.width)
             ]
-            for _ in range(self.height+1)
+            for _ in range(self.height)
         ]
 
     def init_teleporter(self):
-        x, y = rnd.randint(0,self.width-1),rnd.randint(0,self.height-1)
-        x2, y2 =  rnd.randint(0,self.width-1),rnd.randint(0,self.height-1)
-        self.grille[y][x]['teleporter']=True
-        self.grille[y2][x2]['teleporter']=True
+        x, y = rnd.randint(0, self.width-1), rnd.randint(0, self.height-1)
+        x2, y2 = rnd.randint(0, self.width-1), rnd.randint(0, self.height-1)
+        self.grille[y][x]['teleporter'] = True
+        self.grille[y2][x2]['teleporter'] = True
 
-    def is_tp(self,x,y):
-        if self.grille[y][x]['teleporter']:
-            return True
-        return False
-    def is_end(self,x,y):
-        if self.height-1 == y:
-            if self.width-1 == x:
-                return True
+    def is_tp(self, x, y):
+        return self.grille[y][x]['teleporter']
 
-        return False
+    def is_end(self, x, y):
+        return (y == self.height - 1 and x == self.width - 1)
 
     def get_tp(self):
         tp = []
         for y in range(self.height):
             for x in range(self.width):
                 if self.grille[y][x]['teleporter']:
-                    tp.append((x,y))
+                    tp.append((x, y))
         return tp
+
     def remove_tps(self):
-        tps = self.get_tp()
-        for x,y in tps:
-            self.grille[y][x]['teleporter'] = False
+        for y in range(self.height):
+            for x in range(self.width):
+                self.grille[y][x]['teleporter'] = False
         return self
 
     def get_unvisited(self):
-        cells_unvisited = []
-        for y in range(self.height):
-            for x in range(self.width):
-                if not self.grille[y][x]['visited']:
-                    cells_unvisited.append((x, y))
+        cells_unvisited = [(x, y) for y in range(self.height) for x in range(self.width) if not self.grille[y][x]['visited']]
         return cells_unvisited
 
     def delete_wall(self, x, y, direction):
@@ -63,14 +55,10 @@ class Maze:
 
     def get_voisins(self, x, y):
         voisins = []
-        if x > 0:
-            voisins.append((x - 1, y))
-        if x < self.width - 1:
-            voisins.append((x + 1, y))
-        if y > 0:
-            voisins.append((x, y - 1))
-        if y < self.height - 1:
-            voisins.append((x, y + 1))
+        if x > 0: voisins.append((x - 1, y))
+        if x < self.width - 1: voisins.append((x + 1, y))
+        if y > 0: voisins.append((x, y - 1))
+        if y < self.height - 1: voisins.append((x, y + 1))
         return voisins
 
     def is_visited(self, x, y):
@@ -78,111 +66,46 @@ class Maze:
 
     def identify_direction(self, x1, y1, x2, y2):
         if x1 == x2:
-            if y1 == y2 + 1:
-                return 'N', 'S'
-            else:
-                return 'S', 'N'
-        elif x1 == x2 + 1:
-            return 'W', 'E'
-        else:
-            return 'E', 'W'
+            return ('N', 'S') if y1 > y2 else ('S', 'N')
+        return ('W', 'E') if x1 > x2 else ('E', 'W')
 
     def dfs_generation(self, x_debut, y_debut):
-
         self.grille[y_debut][x_debut]['walls']['W'] = False
-        self.grille[self.width - 1][self.height - 1]['walls']['E'] = False
+        self.grille[self.height - 1][self.width - 1]['walls']['E'] = False
         self.grille[y_debut][x_debut]['visited'] = True
         chemin = lifo.creer_pile_vide()
         chemin = lifo.empiler(chemin, (x_debut, y_debut))
+
         while not lifo.est_pile_vide(chemin):
             current = lifo.sommet(chemin)
-            voisins = self.get_voisins(current[0], current[1])
-            unvisited = [voisin for voisin in voisins if not self.is_visited(voisin[0], voisin[1])]
+            unvisited = [v for v in self.get_voisins(*current) if not self.is_visited(*v)]
+
             if unvisited:
                 selected_voisin = rnd.choice(unvisited)
-                murs = self.identify_direction(current[0], current[1], selected_voisin[0], selected_voisin[1])
-                self.delete_wall(current[0], current[1], murs[0])
-                self.delete_wall(selected_voisin[0], selected_voisin[1], murs[1])
+                murs = self.identify_direction(*current, *selected_voisin)
+                self.delete_wall(*current, murs[0])
+                self.delete_wall(*selected_voisin, murs[1])
                 self.grille[selected_voisin[1]][selected_voisin[0]]['visited'] = True
                 chemin = lifo.empiler(chemin, selected_voisin)
             else:
                 chemin = lifo.depiler(chemin)
+
     def prim_algo(self, x_debut, y_debut):
         self.grille[y_debut][x_debut]['walls']['W'] = False
-        self.grille[self.width - 1][self.height - 1]['walls']['E'] = False
-        self.grille[y_debut][x_debut]['visited'] = True        
+        self.grille[self.height - 1][self.width - 1]['walls']['E'] = False
+        self.grille[y_debut][x_debut]['visited'] = True
         frontier = self.get_voisins(x_debut, y_debut)
+
         while frontier:
-            cell = rnd.choice(frontier)
-            x, y = cell[0], cell[1]
-            visited_neighbors = [voisin for voisin in self.get_voisins(x, y)if self.is_visited(voisin[0], voisin[1])]
+            x, y = rnd.choice(frontier)
+            visited_neighbors = [v for v in self.get_voisins(x, y) if self.is_visited(*v)]
             if visited_neighbors:
                 cell2 = rnd.choice(visited_neighbors)
-                wall1, wall2 = self.identify_direction(x, y, cell2[0], cell2[1])
+                wall1, wall2 = self.identify_direction(x, y, *cell2)
                 self.delete_wall(x, y, wall1)
-                self.delete_wall(cell2[0], cell2[1], wall2)
+                self.delete_wall(*cell2, wall2)
             self.grille[y][x]['visited'] = True
-            frontier.remove(cell)
+            frontier.remove((x, y))
             for voisin in self.get_voisins(x, y):
-                vx, vy = voisin[0], voisin[1]
-                if not self.is_visited(vx, vy) and voisin not in frontier:
+                if not self.is_visited(*voisin) and voisin not in frontier:
                     frontier.append(voisin)
-
-def print_labyrinth(maze):
-    for y in range(maze.height):
-        top_line = ""
-        for x in range(maze.width):
-            if maze.grille[y][x]['walls']['N']:
-                top_line += "+---"
-            else:
-                top_line += "+   "
-        top_line += "+"
-        print(top_line)
-
-        mid_line = ""
-        for x in range(maze.width):
-            if maze.grille[y][x]['walls']['W']:
-                mid_line += "|   "
-            else:
-                mid_line += "    "
-        if maze.grille[y][-1]['walls']['E']:
-            mid_line += "|"
-        else:
-            mid_line += " "
-        print(mid_line)
-
-    bottom_line = ""
-    for x in range(maze.width):
-        if maze.grille[maze.height - 1][x]['walls']['S']:
-            bottom_line += "+---"
-        else:
-            bottom_line += "+   "
-    bottom_line += "+"
-    print(bottom_line)
-
-def generate_and_print_labyrinth(width, height, start_x=0, start_y=0):
-    maze = Maze(width, height)
-    maze.init_labyrinth()
-    maze.grille[start_y][start_x]['visited'] = True
-    maze.prim_algo(start_x, start_y)
-    print_labyrinth(maze)
-    return maze
-
-if __name__ == "__main__":
-    seed = input("Entrer une seed ou laisser vide: ")
-    width = input("Entrer la taille en largeur du labyrinth: ")
-    height = input("Entrer la taille en longueur du labyrinth: ")
-    if seed == "":
-        seed = rnd.randint(1, 100000000000000)
-        print(f"Seed: {seed}")
-    rnd.seed(seed)
-
-    maze = generate_and_print_labyrinth(int(width), int(height), 0, 0)
-
-    save_choice = input("Voulez-vous enregistrer le labyrinthe ? (o/n) : ")
-    if save_choice.lower() == 'o':
-        filename = input("Entrez le nom du fichier (ex: labyrinthe.bin) : ")
-        with open(filename, "wb") as file:
-            pickle.dump(maze, file)
-        print(f"Labyrinthe enregistré sous {filename}")
-
